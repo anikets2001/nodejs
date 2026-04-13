f18f15b7e2f6ce1cfa3c0329e43c4c8ceecfe182
Aniket Singh <aniketsingh90126@gmail.com>
2026-04-13 08:21:16 +0530

    feat: server in nodejs

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
+const os = require('os');
 const {add} = require('./01_introduction/math');
 
 console.log('Hello World!');
 
-const addition = add(2,3);
+console.log(os.cpus().length);
+
+const addition = add(2,3); 
 // console.log(`The addition of 2 and 3 is ${addition}`);
+
